a906de43ed9c2cb7706c0cda7c5db8f21913121f
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-03-06 13:41:09 -0600

    port to godot

--- poker/obj/Debug/net9.0/poker.AssemblyInfo.cs
+++ poker/obj/Debug/net9.0/poker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("poker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67b35e67a84d1d46b1976617de5d82932463aedc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cfa9edbf118cc2875dcc1f61c7ea5d1f0d9f88d")]
 [assembly: System.Reflection.AssemblyProductAttribute("poker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("poker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
