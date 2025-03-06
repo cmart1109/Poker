c6fe78f47f1e80e02276952a3b29c0396314a68c
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-03-06 14:19:28 -0600

    dragging cards

---
+++ pokerface/assets/scripts/card_manager.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+func _input(event):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		if event.pressed:
+			raycast_check_for_card()
+		else:
+			print("Left Click Release")
+
+func raycast_check_for_card():
+	var space_state = get_world_2d().direct_space_state
+	var parameters = PhysicsPointQueryParameters2D.new();
+	parameters.position = get_global_mouse_position()
+	parameters.collide_with_areas = true
+	parameters.collision_mask = 1
+	var result = space_state.intersect_point(parameters)
+	if result.size() > 0:
+		return result[0].collider.get_parent()
+	return null
+
+
+func _ready() -> void:
+	pass
+	
+
+func _process(delta: float) -> void:
+	pass
